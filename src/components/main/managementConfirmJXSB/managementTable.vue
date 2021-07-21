a585bbcf33e207610e2cc70c62a5dee5aaa6d4fd
宁静的春天 <oncwnuJfBGQp2MCWyCf9PV4YjtDs@git.weixin.qq.com>
2021-07-21 17:26:52 +0800

    1.完善资方价值确认,还差提交价值确认接口

--- src/components/main/managementConfirmJXSB/managementTable.vue
+++ src/components/main/managementConfirmJXSB/managementTable.vue
@@ -27,10 +27,10 @@
                        label="操作"
                        width="120">
         <template slot-scope="scope">
-          <el-button v-if="scope.row.state==2" @click="priceConfirmView(scope.$index, scope.row)" type="text"
+          <el-button v-if="scope.row.state==2" @click="priceConfirmView(scope.row)" type="text"
                      size="small">查看确认价格
           </el-button>
-          <el-button v-if="scope.row.state==0" @click="priceConfirmView(scope.$index, scope.row)" type="text"
+          <el-button v-if="scope.row.state==0" @click="priceConfirmView(scope.row)" type="text"
                      size="small">价格确认
           </el-button>
         </template>
@@ -48,10 +48,12 @@
           <el-col :span="12"><span class="text">待确认：</span>{{mechanicsData.waitConfirmNum}}</el-col>
         </el-row>
         <el-row>
-          <el-col :span="12"><span class="text">确认放款总价值：</span>{{mechanicsData.totalLoanValue}}</el-col>
-          <el-col :span="12"><span class="text">确认放款总金额：</span>{{mechanicsData.totalLoanAmount}}</el-col>
+          <el-col :span="12"><span class="text">确认放款总价值：</span>{{mechanicsData.totalLoanValue}}元</el-col>
+          <el-col :span="12"><span class="text">确认放款总金额：</span>{{mechanicsData.totalLoanAmount}}元</el-col>
         </el-row>
-        <el-button v-if="rowdata.state==0" type="primary" round style="float: left;margin:10px auto">提交价值确认</el-button>
+        <el-button v-if="rowdata.state==0" type="primary" round style="float: left;margin:10px auto"
+                   @click="confirmPriceSubmit">提交价值确认
+        </el-button>
         <el-table :data="mechanicsData.managementAssessVoList"
                   border
                   stripe
@@ -89,24 +91,26 @@
         </el-table>
       </div>
     </el-drawer>
-    <el-dialog title="确认价格" :visible.sync="dialogVisible" class="confirmDia" width="600px">
+    <el-dialog title="确认价格" :visible.sync="dialogVisible" class="confirmDia" width="600px"
+               :close-on-click-modal='false' :before-close="cancel">
       <el-row>
-        <span>评估价值：</span>
+        <span style="margin-right: 10px">评估价值：</span>{{confirmRowData.mechanicalEvaluationValue}}元
       </el-row>
       <el-row style="margin-top: 20px;">
-        <span>回购价值：</span>
+        <span style="margin-right: 10px">回购价值：</span>{{confirmRowData.mechanicalEvaluationValue}}元
       </el-row>
       <el-row>
-        <el-form :model="confirmData">
-          <el-form-item label="放款价值：" prop="loanValue">
-            <el-input v-model="loanValue" style="width: 300px"></el-input>
-          </el-form-item>
-        </el-form>
+        <span style="margin-right: 10px">放款价值：</span>
+        <el-input :class="loanInput?'':'inputError'" @keyup.native="loanEnter" type="number"
+                  v-model="loanValue"
+                  style="width: 300px"></el-input>
+        <p v-show="tipV" class="tips">请输入放款价值</p>
+        <p v-show="tipV2" class="tips">放款价值不能小于0</p>
       </el-row>
       <div>
         <span class="dialog-footer">
             <el-button @click="cancel()">取 消</el-button>
-            <el-button type="primary" @click="add_submit">确 定</el-button>
+            <el-button type="primary" @click="price_submit">确 定</el-button>
         </span>
       </div>
     </el-dialog>
@@ -124,12 +128,15 @@
         drawerLoad: false,
         mechanicsData: [],
         rowdata: {},
-        confirmData: {},
+        confirmRowData: {},
+        loanValue: '',
+        tipV: false,
+        tipV2: false,
+        loanInput: true,
       }
     },
     methods: {
-      priceConfirmView(index, rowData) {
-        console.log(rowData);
+      priceConfirmView(rowData) {
         this.drawer = true;
         this.drawerLoad = true;
         this.rowdata = JSON.parse(JSON.stringify(rowData));
@@ -139,12 +146,67 @@
         })
       },
       priceConfirm(index, rowData) {
-        console.log(rowData);
+        this.confirmRowData = JSON.parse(JSON.stringify(rowData));
+        this.loanValue = rowData.loanValue ? rowData.loanValue : '';
         this.dialogVisible = true;
       },
-      add_submit() {
-        console.log(this.confirmData);
-      }
+      loanEnter() {
+        this.loanInput = true;
+        this.tipV = false;
+      },
+      price_submit() {
+        if (this.loanValue && parseInt(this.loanValue) > 0) {
+          this.$axios.post('carloan/management_assess/mechanicsSave', {
+            'id': this.confirmRowData.id,
+            'loanValue': this.loanValue
+          }).then((response) => {
+            if (response.data.code === 200) {
+              this.$message({
+                message: '保存成功',
+                type: 'success'
+              });
+              this.tipV = false;
+              this.tipV2 = false;
+              this.dialogVisible = false;
+              this.priceConfirmView(this.rowdata);
+            }
+          })
+        } else {
+          if (parseInt(this.loanValue) < 0) {
+            this.tipV = false;
+            this.tipV2 = true;
+          } else {
+            this.loanInput = false;
+            this.tipV = true;
+          }
+        }
+      },
+      confirmPriceSubmit() {
+        console.log(this.rowdata);
+        let flag = false;
+        this.mechanicsData.managementAssessVoList.map((item) => {
+          console.log(item);
+          if (item.state == 0) {
+            flag = true;
+          }
+        })
+        if (flag) {
+          this.$confirm('还有未确认价格的设备，是否继续?', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+          }).then(()=>{
+            // this.$axios.post('')
+          })
+        }
+      },
+      cancel() {
+        this.loanValue = '';
+        this.dialogVisible = false;
+        this.tipV = false;
+        this.tipV2 = false;
+        this.loanInput = true;
+      },
     }
   }
 </script>
@@ -165,5 +227,16 @@
   .confirmDia .el-row {
     text-align: left;
     margin: 10px 20px;
+  }
+
+  .tips {
+    font-size: 8px;
+    color: red;
+    margin-left: 85px;
+  }
+
+  .inputError {
+    border: red 1px solid;
+    border-radius: 4px;
   }
 </style>
